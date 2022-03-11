db985ae08d0664b36c5bac7cfb94227cea383970
PedroRi99 <94012951+PedroRi99@users.noreply.github.com>
2022-03-11 16:49:54 -0300

    20220311_01

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase:{
+  apiKey: 'AIzaSyD4UjeJJNix5HpT8vmz-07d_Alk3pKtGDI',
+  authDomain: 'app01-fabc6.firebaseapp.com',
+  projectId: 'app01-fabc6',
+  storageBucket: 'app01-fabc6.appspot.com',
+  messagingSenderId: '27811006534',
+  appId: '1:27811006534:web:7ad2c3b7df62d34466a247'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+  apiKey: 'AIzaSyD4UjeJJNix5HpT8vmz-07d_Alk3pKtGDI',
+  authDomain: 'app01-fabc6.firebaseapp.com',
+  projectId: 'app01-fabc6',
+  storageBucket: 'app01-fabc6.appspot.com',
+  messagingSenderId: '27811006534',
+  appId: '1:27811006534:web:7ad2c3b7df62d34466a247'
+  }
 };
 
 /*
